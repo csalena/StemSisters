a496a54d76bda28fb44c7af545b3ed9b988d7d1e
csalena <csatloslena@gmail.com>
2026-06-14 17:06:04 +0200

    feat: StemSisters project are finished for the presentation

--- src/components/Tops.jsx
+++ src/components/Tops.jsx
@@ -1,10 +1,17 @@
 "use client";
 import "../css/Tops.css";
 import { useApiResource } from "../hooks/useApiResource";
+import "animate.css";
+import { Texteffect } from "../hooks/TextEffects.js";
+import { useEffect } from "react";
 
 function Tops() {
   const { data, loading, error } = useApiResource("/api/tops");
   const tops = data[0];
+
+  useEffect(() => {
+    return Texteffect();
+  });
 
   if (loading || !tops) {
     return null;
@@ -24,8 +31,10 @@ function Tops() {
             d="M0,192L48,192C96,192,192,192,288,213.3C384,235,480,277,576,266.7C672,256,768,192,864,181.3C960,171,1056,213,1152,240C1248,267,1344,277,1392,282.7L1440,288L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
           ></path>
         </svg>
-        <h2 id="top3">{tops.sectionTitle}</h2>
-        <div id="cards">
+        <h2 id="top3" class="animate-on-scroll">
+          {tops.sectionTitle}
+        </h2>
+        <div id="cards" class="animate-on-scroll">
           {tops.cards?.map((card) => (
             <article id={card.id} key={card.id}>
               <figure>
